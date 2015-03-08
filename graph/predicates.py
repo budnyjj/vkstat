8643b1574b8cfd3dfb145c2978a2f55bb2da2c98
Raman Budny <budnyjj@gmail.com>
2015-03-08 16:32:29 +0300

    update predicates

--- graph/predicates.py
+++ graph/predicates.py
@@ -4,14 +4,16 @@
 def is_media_activist(node, avg_num_friends):
     '''Check, if person is media-activist.
     Media-activist is a person, who:
-    1. has number of friends more than 5 * average number of friends
+    1. has number of friends more than 3 * average number of friends
     or
-    2. has number of friends more than 2 * average number of friends and
+    2. has number of followers more than 3 * average number of friends
+    or
+    3. has number of friends more than 2 * average number of friends and
     number of his friends is more than 5 * number of his followers.'''
     hard_limit = avg_num_friends * 3
     soft_limit = avg_num_friends * 2
     soft_ff_ratio = 5
-    hard_ff_ratio = 10
+    # hard_ff_ratio = 10
 
     if 'friends_total' in node[1]:
         num_friends = node[1]['friends_total']
@@ -24,10 +26,10 @@ def is_media_activist(node, avg_num_friends):
             elif ((num_followers > soft_limit) and
                   (num_followers > num_friends * soft_ff_ratio)):
                 return True
-            elif num_friends > num_followers * hard_ff_ratio:
-                return True
-            elif ((num_friends > soft_limit) and
-                  (num_friends > num_followers * soft_ff_ratio)):
-                return True
+            # elif num_friends > num_followers * hard_ff_ratio:
+            #     return True
+            # elif ((num_friends > soft_limit) and
+            #       (num_friends > num_followers * soft_ff_ratio)):
+            #     return True
 
     return False
